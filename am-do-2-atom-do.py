0869b739a152a15762504509aecacd62fe99049f
Peter Van Garderen <peter@artefactual.com>
2021-04-27 18:51:17 -0700

    download METS files

--- am-do-2-atom-do.py
+++ am-do-2-atom-do.py
@@ -1,29 +1,50 @@
 
+import os
 import pymysql.cursors
+import requests
+
+# Set Archivematica Storage Service parameters.
+STORAGE_SERVICE_URL = "http://dometadata.analyst.archivematica.net:8000/api/v2/"
+STORAGE_SERVICE_USER = "analyst"
+STORAGE_SERVICE_API_KEY = "ow7ioGh2reephua8uPaiWee4EiHeev6u"
+
+# Set METS download directory.
+METS_DIR = "DIP_METS/"
+
+# Create a working directory for downloading METS files.
+if not os.path.exists(METS_DIR):
+    os.makedirs(METS_DIR)
+
+# Configure AtoM MySQL connection.
+mysqlConnection = pymysql.connect(
+    host="localhost",
+    user="atom-user",
+    password="ATOMPASSWORD",
+    db="atom",
+    charset="utf8mb4",
+    cursorclass=pymysql.cursors.DictCursor,
+)
+mysqlCursor = mysqlConnection.cursor()
+
+# Create a working table for transferring the legacy DIP file properties.
+sql = "CREATE TABLE IF NOT EXISTS dip_files(object_id INTEGER PRIMARY KEY, object_uuid TEXT, aip_uuid TEXT);"
+mysqlCursor.execute(sql)
+mysqlConnection.commit()
+
 
 def main():
+    '''
+    Update pre release 2.7 AtoM digital objects with information from AIP
+    METS to take full advantage of the digital object metadata enhancement and AIP/file retrieval features.
+    '''
 
-    # Configure MySQL connection
-    mysqlConnection = pymysql.connect(
-        host="localhost",
-        user="atom-user",
-        password="ATOMPASSWORD",
-        db="atom",
-        charset="utf8mb4",
-        cursorclass=pymysql.cursors.DictCursor,
-    )
-    mysqlCursor = mysqlConnection.cursor()
-
-    # Create a working table for transferring the legacy DIP file properties.
-    sql = "CREATE TABLE IF NOT EXISTS dip_files(object_id INTEGER PRIMARY KEY, object_uuid TEXT, aip_uuid TEXT);"
-    mysqlCursor.execute(sql)
-    mysqlConnection.commit()
-
-    # Create a working directory for downloading METS files.
+    flush_legacy_digital_file_properties()
+    update_digital_file_properties()
+    delete_temporary_files()
 
 
-
-    # Select all the legacy DIP files so that their properties can be updated.
+def flush_legacy_digital_file_properties():
+    # Select all the legacy DIP files.
     sql = "SELECT * FROM property WHERE name='objectUUID' AND scope is NULL;"
     mysqlCursor.execute(sql)
     legacy_dip_files = mysqlCursor.fetchall()
@@ -67,6 +88,7 @@ def main():
         try:
             # Flush the existing properties for the legacy digital file.
             # Includes an automatic cascade delete of the i18n value.
+            # These properties will be replaced with values from the METS file.
             sql = "DELETE FROM property WHERE object_id = %s;"
             mysqlCursor.execute(sql, file['object_id'])
             mysqlConnection.commit()
@@ -75,13 +97,67 @@ def main():
             print(e)
             continue
 
-        # get all properties for this digital file from the AIP METS file
-        # check if the METS file has been downloaded already
-        # download the METS file
-        # parse the METS file for the property info
-        # write the values to the working table
-        # loop over the working table values and enter updated property values
-        # delete working table. delete METS directory.
+
+def update_digital_file_properties():
+    try:
+        # Select all the legacy DIP file records from the working table.
+        sql = "SELECT * FROM dip_files;"
+        mysqlCursor.execute(sql)
+        legacy_dip_files = mysqlCursor.fetchall()
+    except Exception as e:
+        print("Unable to select files from working table.")
+        print(e)
+
+    for file in legacy_dip_files:
+        # Download METS file if a local copy is not present.
+        if os.path.exists(METS_DIR + file["aip_uuid"] + ".xml") is False:
+            try:
+                path = get_mets_path(file["aip_uuid"])
+            except Exception as e:
+                print("Unable to derive relative path of METS file in " + file["aip_uuid"])
+                print(e)
+            try:
+                get_mets_file(file["aip_uuid"], path)
+            except Exception as e:
+                print("Unable to fetch METS file for " + file["aip_uuid"])
+                print(e)
+
+    # parse the METS file with METSRW for the property info values
+    # write the values to the working table
+    # loop over the working table values and insert updated property values
+
+
+def get_mets_path(aip_uuid):
+    request_url = STORAGE_SERVICE_URL + "file/" + aip_uuid +"?username=" + STORAGE_SERVICE_USER + "&api_key=" + STORAGE_SERVICE_API_KEY
+
+    response = requests.get(request_url)
+    package = response.json()
+
+    # build relative path to METS file
+    if package["current_path"].endswith(".7z"):
+        relativePath = package["current_path"][40:-3]
+    else:
+        relativePath = package["current_path"][40:]
+    relativePathToMETS = (
+        relativePath + "/data/METS." + package["uuid"] + ".xml"
+    )
+
+    return relativePathToMETS
+
+
+def get_mets_file(aip_uuid, relative_path):
+    request_url = STORAGE_SERVICE_URL + "file/" + aip_uuid + "/extract_file/?relative_path_to_file=" + relative_path + "&username=" + STORAGE_SERVICE_USER + "&api_key=" + STORAGE_SERVICE_API_KEY
+    response = requests.get(request_url)
+    mets_file = "METS.{}.xml".format(aip_uuid)
+    download_file = os.path.join(METS_DIR, mets_file)
+    with open(download_file, "wb") as file:
+        file.write(response.content)
+
+
+
+
+
+# delete working table. delete METS directory.
 
 if __name__ == "__main__":
     main()
